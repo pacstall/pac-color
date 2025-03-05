a1cafdb6d62e70b8032fa77bf843f32226b5516d
Elsie <elsie19@pm.me>
2025-03-05 14:17:54 -0500

    Add dynamic file type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use image::{Rgb, RgbImage};
-use rocket::{get, http::ContentType, launch, response::status::BadRequest, routes};
+use image::{ImageFormat, Rgb, RgbImage};
+use rocket::{get, http::ContentType, launch, response::status::BadRequest, routes, FromForm};
 use std::io::Cursor;
+
+#[derive(FromForm)]
+struct ImageSpecifications<'r> {
+    #[field(default = "png")]
+    r#type: &'r str,
+}
+
+fn from_image_format(format: ImageFormat) -> Option<ContentType> {
+    match format {
+        ImageFormat::Png => Some(ContentType::PNG),
+        ImageFormat::Jpeg => Some(ContentType::JPEG),
+        ImageFormat::Gif => Some(ContentType::GIF),
+        ImageFormat::WebP => Some(ContentType::WEBP),
+        ImageFormat::Ico => Some(ContentType::Icon),
+        _ => None,
+    }
+}
 
 fn hex_to_rgb(hex: &str) -> Result<Rgb<u8>, BadRequest<&'static str>> {
     let hex = hex.trim_start_matches('#');
@@ -16,8 +33,13 @@ fn hex_to_rgb(hex: &str) -> Result<Rgb<u8>, BadRequest<&'static str>> {
     Ok(Rgb([r, g, b]))
 }
 
-#[get("/<size>/<color>")]
-fn colorize<'a>(size: &str, color: &'a str) -> Result<(ContentType, Vec<u8>), BadRequest<&'a str>> {
+#[get("/<size>/<color>?<spec..>")]
+#[allow(clippy::needless_pass_by_value)]
+fn colorize(
+    size: &str,
+    color: &str,
+    spec: ImageSpecifications,
+) -> Result<(ContentType, Vec<u8>), BadRequest<&'static str>> {
     let (height, weight) = match size.split_once('x') {
         Some((height, weight)) => {
             let height: u32 = height
@@ -34,6 +56,20 @@ fn colorize<'a>(size: &str, color: &'a str) -> Result<(ContentType, Vec<u8>), Ba
             ))
         }
     };
+
+    // Sanity checks
+    if height > 500 || weight > 500 {
+        return Err(BadRequest("Size exceeds 500 in one direction"));
+    }
+
+    let Some(format) = ImageFormat::from_extension(spec.r#type) else {
+        return Err(BadRequest("Invalid extension"));
+    };
+
+    let Some(content_type) = from_image_format(format) else {
+        return Err(BadRequest("Unsupported extension"));
+    };
+
     let mut img = RgbImage::new(height, weight);
 
     let fill = hex_to_rgb(color)?;
@@ -42,15 +78,15 @@ fn colorize<'a>(size: &str, color: &'a str) -> Result<(ContentType, Vec<u8>), Ba
 
     let mut buffer = Vec::new();
 
-    img.write_to(&mut Cursor::new(&mut buffer), image::ImageFormat::Png)
-        .expect("Failed to write PNG");
+    img.write_to(&mut Cursor::new(&mut buffer), format)
+        .map_err(|_| BadRequest("Could not make image"))?;
 
-    Ok((ContentType::PNG, buffer))
+    Ok((content_type, buffer))
 }
 
 #[get("/")]
 fn splash() -> &'static str {
-    "Use the slug: `/FFFFFF` for example."
+    "Use the slug: `/100x100/FFFFFF` for example."
 }
 
 #[launch]
